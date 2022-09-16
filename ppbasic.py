4e366ad2f09ef1e9f2874c9748d92312ac15f4e0
Sean Gugler <sean.gugler@gmail.com>
2022-09-16 01:54:24 -0700

    improve ppbasic: generate dependency graph; make upper-case optional

--- ppbasic.py
+++ ppbasic.py
@@ -5,41 +5,54 @@ including Applesoft, TRS-80, and Commodore
 
 Features:
 * Automatic numbering
+* Upper-case conversion
 * Ignore empty lines and #comments
 * Concatenate lines with :line
 * Symbolic labels with @name
 * Conditionally-excluded blocks
+* Dependency graph; viewer at http://viz-js.com/
 
 Created by Sean Gugler 2022-08-06
-last revised 2022-08-22
+last revised 2022-09-16
 """
 import sys
 import re
+import argparse
 from pathlib import Path
 
 patLabel = re.compile(r'@+\w+')
 
+def usage():
+    parser = argparse.ArgumentParser(description = __doc__.split('\n')[0])  # extract first line of """ header
+    parser.add_argument('basic', help="Input file")
+    parser.add_argument('-c', '--case', action='store_true', help="Convert all lines to uppercase")
+    parser.add_argument('-d', '--deps', action='store_true', help="Generate dependency graph in Graphviz DOT format")
+    return parser.parse_args()
+
 def main(infile):
     file = Path(infile)
     lines = file.read_text().split('\n')
-    caps,labels = pass1(lines)
-    fixup = pass2(caps,labels)
+
+    numbered,labels = pass1(lines)
+    fixup,deps = pass2(numbered,labels)
+
     file.with_suffix('.txt').write_text('\n'.join(fixup)+'\n')
+    if args.deps:
+        file.with_suffix('.dep').write_text('\n'.join(fmt_dot(deps))+'\n')
     return 0
 
 def round_up(n,m):
     return ((n//m)+1)*m
 
 def pass1(raw):
-    """Convert to BASIC syntax
-    Concatenate lines, skip comments, mark labels, make uppercase
-    """
+    """Auto-number, and apply all pre-processing directives."""
     out = []
     label = {}
     disable = {}
     n = 0
     skip = False
-    for line in map(str.upper, raw):
+    src = map(str.upper, raw) if args.case else raw
+    for line in src:
         if not line or line.startswith('#'):
             continue
         elif line.startswith('!'):
@@ -51,6 +64,7 @@ def pass1(raw):
         elif skip:
             continue
         elif line.startswith('@'):
+            here = line
             if line[1] != '@':
                 n = round_up(n,100)
             if line in label:
@@ -66,9 +80,31 @@ def pass1(raw):
     return out,label
 
 def pass2(bas,label):
-    """Fixup labels"""
-    for n,line in enumerate(bas):
-        yield patLabel.sub (lambda m: label[m.group(0)], line)
+    """Fixup labels, create dependency graph"""
+    dep = []
+    here = 'TOP'
+    def line_num(match):
+        jump = match.group(0)
+        dep.append(map(lambda s: s.lstrip('@'), (here, jump)))
+        return label[jump]
+
+    fixed = []
+    section = {v:k for k,v in label.items()}
+    for line in bas:
+        n = line.split(' ',1)[0]
+        here = section.get(n, here)
+        fixed.append(patLabel.sub (line_num, line))
+    return fixed, dep
+
+def fmt_dot(deps):
+    """Format dependencies in DOT syntax"""
+    yield 'strict digraph {'
+    yield '\trankdir="LR"'
+    yield '\tranksep=2'
+    for a,b in deps:
+        yield f'\t{a} -> {b}'
+    yield '}'
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1]))
+    args = usage()
+    sys.exit(main(args.basic))
